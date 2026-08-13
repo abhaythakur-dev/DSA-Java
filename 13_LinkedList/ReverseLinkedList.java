5ace7901621866a8b403e14f21fb8e7c926ae27e
abhaythakur730244-ops <abhaythakur730244@gmail.com>
2026-08-13 13:48:21 +0530

    Add LinkedList

---
+++ 13_LinkedList/ReverseLinkedList.java
@@ -0,0 +1,55 @@
+public class ReverseLinkedList {
+
+    public static Node reverseList(Node head) {
+
+        // Using 3 Pointer 
+        // previous 
+        // current 
+        // next
+        
+
+        Node prev = null;
+        Node curr = head;
+
+        while (curr != null) {
+
+            Node next = curr.next;
+
+            curr.next = prev;
+
+            prev = curr;
+            curr = next;
+        }
+
+        return prev;
+    }
+
+    public static void printList(Node t) {
+
+        while (t != null) {
+            System.out.print(t.val + " -> ");
+            t = t.next;
+        }
+
+        System.out.println("null");
+    }
+
+    public static void main(String[] args) {
+
+        // 1 -> 2 -> 3 -> 4 -> 5
+
+        Node head = new Node(1);
+        head.next = new Node(2);
+        head.next.next = new Node(3);
+        head.next.next.next = new Node(4);
+        head.next.next.next.next = new Node(5);
+
+        System.out.println("Before:");
+        printList(head);
+
+        head = reverseList(head);
+
+        System.out.println("After:");
+        printList(head);
+    }
+}
